1535bf398379ecd292e529d249d8900016b13ffc
PC Kos <mbandrigo@gmail.com>
2025-05-17 07:29:37 +0800

    detail

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 
-Route::get('/', function () {
-    return view('landing');
-});
+Route::get('/', fn() => view('home'));
+Route::get('/detail/{id}', fn() => view('detail'))->name('detail');
 Route::get('/my-area', fn() => view('admin.login'))->name('login.page');
 Route::get('/produk', fn() => view('admin.product-data'))->name('product.data');
 Route::get('/produk/tambah', fn() => view('admin.product-add'))->name('product.add');
@@ -13,21 +12,3 @@ Route::get('/produk/{id}/edit', fn() => view('admin.product-edit'))->name('produ
 
 Route::get('/dashboard', fn() => view('admin.dashboard'))->name('dashboard');
 Route::get('/penjualan', fn() => view('admin.penjualan'))->name('penjualan');
-
-Route::get('/run-migrate-seed', function () {
-    Artisan::call('migrate --seed');
-    return 'Migrate and Seed Completed';
-});
-
-Route::get('/run-storage-link', function () {
-    Artisan::call('storage:link');
-    return 'Storage Link Created';
-});
-
-Route::get('/run-jwt-key', function () {
-    // Set JWT key, pastikan hanya diakses dengan validasi atau token yang aman.
-    if (!env('JWT_SECRET')) {
-        Artisan::call('key:generate --env=production');
-    }
-    return 'JWT Key Generated or Already Set';
-});

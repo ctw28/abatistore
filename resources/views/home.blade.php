d9cab99833fbe6aadcc97d82d1ed556bca59d965
ctw28 <mbandrigo@gmail.com>
2026-01-10 09:37:33 +0800

    perbaikan gambar path

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -408,7 +408,7 @@
 
                                 <img class="img-fluid" :src="getImageUrl(product.image)" alt="..." />
                                 <!-- Overlay jika produk habis -->
-                                <div v-if="parseInt(product.is_habis) === 1" class="position-absolute top-50 start-50 translate-middle d-flex
+                                <div v-if="product.is_habis" class="position-absolute top-50 start-50 translate-middle d-flex
                                     justify-content-center align-items-center" style="width: 80px; height: 80px; border-radius: 50%; background-color: rgba(0, 0,
                                     0, 0.7); color: white; font-weight: bold; font-size: 0.8rem;">
                                     Habis
@@ -458,7 +458,7 @@
                                 >
                                 <img class="img-fluid" :src="getImageUrl(product.image)" alt="..." />
                                 <!-- Overlay jika produk habis -->
-                                <div v-if="parseInt(product.is_habis) === 1" class="position-absolute top-50 start-50 translate-middle d-flex
+                                <div v-if="product.is_habis" class="position-absolute top-50 start-50 translate-middle d-flex
                                     justify-content-center align-items-center" style="width: 80px; height: 80px; border-radius: 50%; background-color: rgba(0, 0,
                                     0, 0.7); color: white; font-weight: bold; font-size: 0.8rem;">
                                     Habis

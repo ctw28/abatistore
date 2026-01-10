8cef43c224988733f1ce674f072645fd1946224e
ctw28 <mbandrigo@gmail.com>
2026-01-10 09:22:26 +0800

    perbaikan gambar path

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -408,10 +408,8 @@
 
                                 <img class="img-fluid" :src="getImageUrl(product.image)" alt="..." />
                                 <!-- Overlay jika produk habis -->
-                                <div v-if="parseInt(product.is_habis) === 1">
-                                    class="position-absolute top-50 start-50 translate-middle d-flex
-                                    justify-content-center align-items-center"
-                                    style="width: 80px; height: 80px; border-radius: 50%; background-color: rgba(0, 0,
+                                <div v-if="parseInt(product.is_habis) === 1" class="position-absolute top-50 start-50 translate-middle d-flex
+                                    justify-content-center align-items-center" style="width: 80px; height: 80px; border-radius: 50%; background-color: rgba(0, 0,
                                     0, 0.7); color: white; font-weight: bold; font-size: 0.8rem;">
                                     Habis
                                 </div>
@@ -460,10 +458,8 @@
                                 >
                                 <img class="img-fluid" :src="getImageUrl(product.image)" alt="..." />
                                 <!-- Overlay jika produk habis -->
-                                <div v-if="parseInt(product.is_habis) === 1">
-                                    class="position-absolute top-50 start-50 translate-middle d-flex
-                                    justify-content-center align-items-center"
-                                    style="width: 80px; height: 80px; border-radius: 50%; background-color: rgba(0, 0,
+                                <div v-if="parseInt(product.is_habis) === 1" class="position-absolute top-50 start-50 translate-middle d-flex
+                                    justify-content-center align-items-center" style="width: 80px; height: 80px; border-radius: 50%; background-color: rgba(0, 0,
                                     0, 0.7); color: white; font-weight: bold; font-size: 0.8rem;">
                                     Habis
                                 </div>

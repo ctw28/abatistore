799f0ad47ec29e330f91c4115d3a40281183f09f
ctw28 <mbandrigo@gmail.com>
2026-08-17 16:43:30 +0800

    promo179

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -703,7 +703,7 @@
                     return 15;
                 }
                 if (product.category?.name.toLowerCase() === 'celana') {
-                    return 10;
+                    return 17;
                 }
 
                 return 5;
